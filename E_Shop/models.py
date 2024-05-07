e3a02e03d3dc22aef825447d5308080385e3719c
BaroNhat <nguyenbaonhat06@gmail.com>
2024-05-08 01:38:55 +0700

    Create Product Model

--- E_Shop/models.py
+++ E_Shop/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from django.utils import timezone
 class Categoires(models.Model):
     name = models.CharField(max_length=200)
     
@@ -21,3 +21,30 @@ class Filter_Price(models.Model):
         ('40000 TO 50000', '40000 TO 50000'),
     )
     price = models.CharField(choices=FILTER_PRICE, max_length=60)
+
+class Product(models.Model):
+    CONDITION = (('New', 'New'), ('Old','Old'))
+    STOCK = ('IN STOCK', 'IN STOCK'), ('OUT OF STOCK', 'OUT OF STOCK')
+    STATUS = ('Publish', 'Publish'), ('Draft', 'Draft')
+
+    unique_id = models.CharField(unique=True, max_length=200, null=False, blank=True)
+    image = models.ImageField(upload_to='Product_images/img')
+    name = models.CharField(max_length=200)
+    price = models.IntegerField()
+    condition = models.CharField(choices=CONDITION, max_length=100)
+    information = models.TextField()
+    description = models.TextField()
+    stock = models.CharField(choices=STOCK, max_length=200)
+    status = models.CharField(choices=STATUS, max_length=200)
+    created_date = models.DateTimeField(default=timezone.now)
+
+    categories = models.ForeignKey(Categoires, on_delete=models.CASCADE)
+    brand = models.ForeignKey(Brand, on_delete=models.CASCADE)
+    color = models.ForeignKey(Color, on_delete=models.CASCADE)
+    filter_price = models.ForeignKey(Filter_Price, on_delete=models.CASCADE)
+
+
+    def save(self, args, **kwargs):
+        if self.unique_id is None and self.created_date and self.id:
+            self.unique_id = self.created_date.strftime('75%Y%m%d24') + str(self.id)
+        return super().save(*args, **kwargs)
