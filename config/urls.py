015a8ab4a1104c5d1b495217d08252d208b601ef
LaDangHuy <lahuy136@gmail.com>
2024-05-09 14:17:45 +0700

    Update Search & Sort Product

--- config/urls.py
+++ config/urls.py
@@ -9,7 +9,8 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("", views.HOME, name='home'),
+    path('', views.HOME, name='home'),
     path('base/', views.BASE,name='base'),  
     path('products/',views.PRODUCT, name='products'),
+    path('search/', views.SEARCH, name='search'),
 ] + static(settings.MEDIA_URL,document_root = settings.MEDIA_ROOT)
